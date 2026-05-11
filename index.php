275e2d8fd065229e69699153f225b8bc24e038c1
Gemma-1 <gemagithub@gmail.com>
2026-05-11 21:37:22 +0200

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 echo "<h1>Proyecto Ampliación Sistemas Informáticos</h1>";
-echo "<h1>Versión con actualización automática</h1>";
+
+echo "<h1>Integrantes:";
+echo "<h2>Giulia Alisia</h2>";
+echo "<h2>Marta Pérez</h2>";
+echo "<h2>Laura Moreno</h2>";
+
+echo "<p>Desplegado automáticamente desde GitHub.</p>";
 ?>
